406441ebb50d1b6029e7d0d61bbd432ad5031f4d
Terry Park <terrypark@Terrys-MacBook-Air.local>
2023-09-24 15:40:27 -0700

    movie seat booking project in progress

--- movie_seat_booking/script.js
+++ movie_seat_booking/script.js
@@ -6,8 +6,18 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice){
+  localStorage.setItem('selectedMovieIndex', movieIndex)
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat))
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
   const selectedSeatsCount = selectedSeats.length;
 
   count.innerText = selectedSeatsCount
@@ -17,6 +27,7 @@ function updateSelectedCount() {
 
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value)
   updateSelectedCount();
 })
 
