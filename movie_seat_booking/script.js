360555f52e8d1b03e089bfa80c24c4df83a6f9d3
Terry Park <terrypark@Terrys-MacBook-Air.local>
2023-09-25 17:16:46 -0700

    movie seat booking project finished

--- movie_seat_booking/script.js
+++ movie_seat_booking/script.js
@@ -4,6 +4,8 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 function setMovieData(movieIndex, moviePrice){
@@ -24,6 +26,23 @@ function updateSelectedCount() {
   total.innerText = selectedSeatsCount * ticketPrice
 }
 
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'))
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
+
+}
+
 
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = e.target.value;
@@ -39,3 +58,5 @@ container.addEventListener('click', (e) => {
     updateSelectedCount();
   }
 })
+
+updateSelectedCount()
