24b46dc90342434d412553298043f42a3da8d0dc
Terry Park <terrypark@Terrys-MacBook-Air.local>
2023-11-25 14:19:49 -0800

    breakout game project finished

--- breakout_game/script.js
+++ breakout_game/script.js
@@ -102,6 +102,76 @@ function movePaddle() {
   }
 }
 
+// Move ball on canvas
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  // Wall collision (right/left)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+
+  // Wall collision (top/bottom)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  // Paddle collision
+  if (
+    ball.x - ball.size > paddle.x && 
+    ball.x + ball.size < paddle.x + paddle.w && 
+    ball.y + ball.size > paddle.y
+    ) {
+      ball.dy = -ball.speed;
+  }
+
+  // Brick collision
+bricks.forEach(column => {
+  column.forEach(brick => {
+    if (brick.visible) {
+      if (
+        ball.x - ball.size > brick.x && // left brick side check
+        ball.x + ball.size < brick.x + brick.w && // right brick side check
+        ball.y + ball.size > brick.y && // top brick side check
+        ball.y - ball.size < brick.y + brick.h // bottom brick side check
+      ) {
+        ball.dy *= -1;
+        brick.visible = false;
+
+        increaseScore();
+        }
+    }
+  })
+})
+
+  // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+
+}
+
+
+
+// Increase Score
+function increaseScore() {
+  score++;
+  if(score % (brickRowCount * brickColumnCount) === 0) {
+    showAllBricks();
+  }
+}
+
+// Make all bricks appear
+function showAllBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => {
+      brick.visible = true;
+    })
+  })
+}
+
 // Draw everything
 
 function draw() {
@@ -117,6 +187,7 @@ function draw() {
 // Update canvas drawing and animation
 function update() {
   movePaddle();
+  moveBall();
 
   // Draw everything
   draw();
